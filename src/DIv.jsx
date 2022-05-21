f44188bf9789b45fe746065d72235a94c4347f34
[frontend09] <ylee981352@gmail.com>
2022-05-21 23:58:02 +0900

    [#1] 실습 내용 수정 및 추가

--- src/DIv.jsx
+++ src/DIv.jsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-const Div = ({ children, name1 }: { name1: string; children?: ReactNode }) => {
+const Div = ({ children, name1 }: { name1: string, children?: ReactNode }) => {
   return (
     <div>
       div: {name1} <br />
